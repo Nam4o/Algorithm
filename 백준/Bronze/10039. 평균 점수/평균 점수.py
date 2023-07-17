212a5df89cd0dde68c9dc90f2b61a708931178a1
Jeonghw4n <128338647+Nam4o@users.noreply.github.com>
2023-07-17 22:46:26 +0900

    [Bronze IV] Title: 평균 점수, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -0,0 +1,16 @@
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+E = int(input())
+if A <= 40:
+    A = 40
+if B <= 40:
+    B = 40
+if C <= 40:
+    C = 40
+if D <= 40:
+    D = 40
+if E <= 40:
+    E = 40
+print((A + B + C + D +E)//5)
