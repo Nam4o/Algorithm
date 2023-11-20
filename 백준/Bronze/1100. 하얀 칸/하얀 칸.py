893ceaf64b8c0f423d6bcc250ffecfbe613c92f7
Jeonghw4n <128338647+Nam4o@users.noreply.github.com>
2023-11-20 21:09:35 +0900

    [Bronze II] Title: 하얀 칸, Time: 124 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,10 @@
+arr = [list(input().strip()) for _ in range(8)]
+
+cnt = 0
+for i in range(8):
+    for j in range(8):
+        if i % 2 == 0 and j % 2 == 0 and arr[i][j] == 'F':
+            cnt += 1
+        elif i % 2 == 1 and j % 2 == 1 and arr[i][j] == 'F':
+            cnt += 1
+print(cnt)
