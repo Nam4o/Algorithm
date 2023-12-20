fb3ecef776a565b60d8ebfe03ae4475e7557c9c0
Jeonghw4n <128338647+Nam4o@users.noreply.github.com>
2023-12-21 02:24:42 +0900

    [level 2] Title: [3차] 방금그곡, Time: 6.87 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -0,0 +1,58 @@
+def solution(m, musicinfos):
+    
+    
+    target = []
+    check = False
+    
+    
+    for f in m:
+        if f != "#":
+            target.append(f)
+        else:
+            target[-1] += f
+            
+            
+    print(target)
+    
+    answer = ""
+    ans = []
+    dictionary = {}
+    cnt = 100
+    for i in musicinfos:
+        i = i.split(",")
+        
+        
+        runtime = (int(i[1][:2]) * 60 + int(i[1][3:])) - (int(i[0][:2]) * 60 + int(i[0][3:]))
+        
+        tmp = []
+        for j in i[3]:
+            if j != "#":
+                tmp.append(j)
+            else:
+                tmp[-1] += j
+        if runtime >= len(tmp):
+            dictionary[i[2]] = [tmp * (runtime // len(tmp)) + tmp[:runtime % len(tmp)], runtime, cnt]
+            
+        else:
+            dictionary[i[2]] = [tmp[:runtime], runtime, cnt]
+        cnt -= 1
+        
+    for t in dictionary:
+        
+        for w in range(len(dictionary[t][0])):
+            if dictionary[t][0][w:w + len(target)] == target:
+                
+                # print(dictionary[t][w:w + len(target)])
+                
+                check = True
+                ans.append([t, dictionary[t][1], dictionary[t][2]])
+    
+    
+    if check == False:
+        answer = "(None)"
+    else:
+        ans.sort(key=lambda x: (x[1], x[2]), reverse=True)
+        
+        answer = ans[0][0]
+    
+    return answer
