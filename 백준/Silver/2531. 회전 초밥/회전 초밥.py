cf39c7097e54fb4f6ae7d89d8e75249ad1bc0456
Jeonghw4n <128338647+Nam4o@users.noreply.github.com>
2024-01-03 22:34:14 +0900

    [Silver I] Title: 회전 초밥, Time: 180 ms, Memory: 114376 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -0,0 +1,63 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+N, d, k, c = map(int, input().split())
+
+arr = [int(input()) for _ in range(N)]
+
+dish = {}
+
+unique_count = 0  # 유니크한 음식 수를 추적
+
+queue = deque()
+
+mx = 0
+
+for i in range(k):
+    queue.append(arr[i])
+    if arr[i] in dish:
+        dish[arr[i]] += 1
+    else:
+        dish[arr[i]] = 1
+        unique_count += 1
+
+
+
+if unique_count >= mx:
+    if c in dish and dish[c] > 0:
+        mx = unique_count
+    else:
+        mx = unique_count + 1
+
+i = k
+
+while i - k < N:
+    if mx == k + 1:
+        break
+    tmp = queue.popleft()
+    dish[tmp] -= 1
+    if dish[tmp] == 0:
+        unique_count -= 1
+
+    queue.append(arr[i % N])
+    if arr[i % N] in dish:
+        if dish[arr[i % N]] == 0:
+            unique_count += 1
+        dish[arr[i % N]] += 1
+
+    else:
+        dish[arr[i % N]] = 1
+        unique_count += 1
+    if unique_count >= mx:
+        if c in dish and dish[c] > 0:
+            mx = unique_count
+        else:
+            mx = unique_count + 1
+
+
+
+    i += 1
+
+print(mx)
