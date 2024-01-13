263ffe09a7c3467a3a4dc3e7cd78272f5c31179d
Jeonghw4n <128338647+Nam4o@users.noreply.github.com>
2024-01-13 15:59:30 +0900

    [level Lv.3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,55 @@
+import heapq, sys
+input = sys.stdin.readline
+
+def solution(operations):
+    answer = []
+    
+    low = []
+    high = []
+    
+    for o in operations:
+        tmp = o.split(" ")
+        tmp[1] = int(tmp[1])
+        # print(tmp[1])
+        if tmp[0] == "I":
+            if tmp[1] < 0:
+                heapq.heappush(low, tmp[1])
+            else:
+                heapq.heappush(high, -tmp[1])
+        else:
+            if tmp[1] == 1:
+                if high:
+                    heapq.heappop(high)
+                else:
+                    if low:
+                        low.remove(max(low))
+            else:
+                if low:
+                    heapq.heappop(low)
+                else:
+                    if high:
+                        high.remove(max(high))
+                    
+
+    if high:
+        answer.append(-heapq.heappop(high))
+        if low:
+            answer.append(heapq.heappop(low))
+        elif high:
+            answer.append(-max(high))
+        else:
+            answer.append(answer[0])
+    elif low:
+        tmp_pop = heapq.heappop(low)
+        if low:
+            answer.append(max(low))
+            answer.append(tmp_pop)
+        else:
+            answer.append(tmp_pop)
+            answer.append(tmp_pop)
+    else:
+        answer = [0, 0]
+            
+            
+        
+    return answer
